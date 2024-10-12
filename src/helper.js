1737e4d52076ff60dc60710dbbef935c37c2646b
Ramzy Syahrul R <ramzy.sramadhan@gmail.com>
2024-10-12 16:03:55 +0700

    remove comment

--- src/helper.js
+++ src/helper.js
@@ -9,13 +9,6 @@ module.exports = {
           },
           body: JSON.stringify({ email, password }),
         });
-      
-        // if (!response.ok) {
-        //   throw new Error(`Error: ${response.status} ${response.statusText}`);
-        // }
-      
-        // const data = await response.json();
-        // console.warn(data);
         
         const data = response;
         return data;
